1c1e2079497e9a0431b37cf92c20b88a79ca2285
Sunil Soni <sunilsoni@live.in>
2018-05-01 11:53:18 +0530

    ConfigurationAnnotationTest

--- src/test/java/com/annotation/spring/Configuration/ConfigurationAnnotationTest.java
+++ src/test/java/com/annotation/spring/Configuration/ConfigurationAnnotationTest.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
-
+   
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Slf4j
