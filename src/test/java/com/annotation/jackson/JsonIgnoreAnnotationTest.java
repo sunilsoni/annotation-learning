510e8a5b3ff62b11776a3f855eddf46c94e4ee7f
Sunil Soni <sunilsoni@live.in>
2018-05-01 11:48:59 +0530

    ConfigurationAnnotationTest

--- src/test/java/com/annotation/jackson/JsonIgnoreAnnotationTest.java
+++ src/test/java/com/annotation/jackson/JsonIgnoreAnnotationTest.java
@@ -1,46 +1,46 @@
 package com.annotation.jackson;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.extern.slf4j.Slf4j;
+import java.io.IOException;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
 
-import java.io.IOException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 class JsonIgnoreAnnotationTest {
 
-    ObjectMapper objectMapper;
+	ObjectMapper objectMapper;
 
-    @BeforeClass
-    void setUp() {
-        objectMapper = new ObjectMapper();
-    }
+	@BeforeClass
+	void setUp() {
+		objectMapper = new ObjectMapper();
+	}
 
-    @AfterEach
-    void tearDown() {
-    }
+	@AfterEach
+	void tearDown() {
+	}
 
-    @Test
-    public void testSerializingWithJsonIgnore()
-            throws JsonProcessingException {
-        final String jsonString = objectMapper.writeValueAsString(new JsonIgnoreAnnotation());
-        System.out.println(jsonString);
+	@Test
+	public void testSerializingWithJsonIgnore() throws JsonProcessingException {
+		final String jsonString = objectMapper.writeValueAsString(new JsonIgnoreAnnotation());
+		log.info(jsonString);
 
+		// Assert.assertThat(jsonString, Matchers.containsString("James Clark"));
+		// Assert.assertThat(jsonString, not(containsString("productId")));
+	}
 
-        // Assert.assertThat(jsonString, Matchers.containsString("James Clark"));
-        // Assert.assertThat(jsonString, not(containsString("productId")));
-    }
-
-    @Test
-    public void testDeSerializingWithJsonIgnore() throws IOException {
-        final String jsonString = "{\"personId\": 231, \"name\": \"Mary Parker\"}";
-        final ObjectMapper mapper = new ObjectMapper();
-        final JsonIgnoreAnnotation bean = objectMapper.readValue(jsonString, JsonIgnoreAnnotation.class);
-        System.out.println(bean);
-        //assertThat(bean.name, is(equalTo("Mary Parker")));
-        // assertThat(bean.personId, is(not(equalTo(231L))));
-    }
+	@Test
+	public void testDeSerializingWithJsonIgnore() throws IOException {
+		final String jsonString = "{\"personId\": 231, \"name\": \"Mary Parker\"}";
+		final ObjectMapper mapper = new ObjectMapper();
+		final JsonIgnoreAnnotation bean = objectMapper.readValue(jsonString, JsonIgnoreAnnotation.class);
+		System.out.println(bean);
+		// assertThat(bean.name, is(equalTo("Mary Parker")));
+		// assertThat(bean.personId, is(not(equalTo(231L))));
+	}
 }
