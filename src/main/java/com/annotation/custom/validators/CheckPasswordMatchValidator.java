e5da86c43c9396311c14ee47fc1bc6ecf44dfb4a
Sunil Soni <sunilsoni@live.in>
2018-05-06 12:40:08 +0530

    custom

--- src/main/java/com/annotation/custom/validators/CheckPasswordMatchValidator.java
+++ src/main/java/com/annotation/custom/validators/CheckPasswordMatchValidator.java
@@ -1,27 +1,23 @@
 package com.annotation.custom.validators;
 
-import com.logos.domain.AddUserRequest;
 import org.springframework.stereotype.Component;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 @Component
-public class CheckPasswordMatchValidator implements ConstraintValidator<CheckPasswordMatch, AddUserRequest> {
+public class CheckPasswordMatchValidator implements ConstraintValidator<CheckPasswordMatch, String> {
 
     @Override
-    public boolean isValid(final AddUserRequest addUserRequest, final ConstraintValidatorContext arg1) {
+    public boolean isValid(final String userRequestPassword, final ConstraintValidatorContext arg1) {
 
 
-        if (addUserRequest.getPassword() == null || addUserRequest.getPasswordConfirmation() == null) {
+        if (userRequestPassword == null) {
             return false;
         }
 
-        if (addUserRequest.getPassword().equals(addUserRequest.getPasswordConfirmation())) {
-            return true;
-        }
 
-        return false;
+        return true;
     }
 
 }
